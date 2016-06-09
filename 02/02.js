4d35a01d8bd95baf2c1fd1033d70939f4847807d
YutoShimizu <yut0707smz.sgsc@gmail.com>
2016-06-09 12:19:01 +0900

    中間試験に回答した

---
+++ 02/02.js
@@ -0,0 +1,15 @@
+function plus(1 , 0){
+   return 1 + 0;
+}
+function minus(1 , 1){
+    return 1 - 1;
+}
+function times(2,4){
+    return 2 * 4;
+}
+function div(8,4){
+    return 8 / 4;
+}
+function mod(8,7){
+    return 8 % 7;
+}
